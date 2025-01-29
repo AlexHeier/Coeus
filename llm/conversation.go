d6d000bd20dedafea8037d90badf9435c09c87ca
JohnHagen <johnandershagen@gmail.com>
2025-01-29 21:34:44 +0100

    ADD: Conversational memory

--- llm/conversation.go
+++ llm/conversation.go
@@ -1,15 +1,59 @@
 package llm
 
+import "Coeus/provider"
+
+var conversations []Conversation
+
 type Conversation struct {
-	MainPrompt string
-	ToolsDesc  string
-	ToolsResp  string
-	History    string
-	UserPrompt string
+	MainPrompt     string
+	ToolsDesc      string
+	ToolsResp      string
+	History        []string
+	LatestResponse string
+	UserPrompt     string
 }
 
-var MainConversation Conversation
+func (c *Conversation) AppendUserHistory(s string) {
+	newHistory := "[USER]\n" + s + "\n\n"
+	c.History = append(c.History, newHistory)
+}
 
-func (c *Conversation) UpdateHistory(s string) {
-	c.History += s
+func (c *Conversation) AppendSystemHistory(s string) {
+	newHistory := "[SYSTEM]\n" + s + "\n\n"
+	c.History = append(c.History, newHistory)
+}
+
+func (c *Conversation) AppendLLMHistory(s string) {
+	newHistory := "[LLM]\n" + s + "\n\n"
+	c.History = append(c.History, newHistory)
+}
+
+func (c *Conversation) Prompt(s string) (map[string]interface{}, error) {
+	prefix := c.MainPrompt + "[BEGIN HISTORY]" + c.DumpConversation() + "[END HISTORY]\n\n"
+	res, err := provider.Send(prefix + s)
+	if err != nil {
+		return res, err
+	}
+	c.AppendUserHistory(s)
+	c.AppendLLMHistory(res["response"].(string))
+	c.LatestResponse = res["response"].(string)
+	return res, err
+}
+
+func (c *Conversation) DumpConversation() string {
+	dump := c.MainPrompt
+
+	for _, h := range c.History {
+		dump += h
+	}
+	return dump
+}
+
+func BeginConversation() *Conversation {
+	newCon := Conversation{
+		MainPrompt: Persona,
+	}
+
+	conversations = append(conversations, newCon)
+	return &conversations[len(conversations)-1]
 }
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"Coeus/llm/memory"
+	"Coeus/llm"
 	"Coeus/provider"
+	"fmt"
 	"log"
 	"os"
 
@@ -23,6 +24,35 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	memory.Version(memory.Summary)
+	con := llm.BeginConversation()
+	con.Prompt("Hi im 10 years old and where does babies come from?")
+
+	fmt.Println(con.LatestResponse)
+
+	//fmt.Println(con.DumpConversation())
+
+	con.Prompt("Yes that does make sence! Thank you. Also what was my previous question?")
+
+	//fmt.Println(con.DumpConversation())
+
+	fmt.Println(con.LatestResponse)
+
+	con.Prompt("Do you have any jokes relevant to the last questions i gave you?")
+
+	fmt.Println(con.LatestResponse)
+
+	con.Prompt("Whats PI's first 150 numbers?")
+
+	fmt.Println(con.LatestResponse)
+
+	con.Prompt("Can you give me a list of the previous questions asked?")
+
+	fmt.Println(con.LatestResponse)
+
+	con.Prompt("What part about your functionality do you think im testing?")
+
+	fmt.Println(con.LatestResponse)
+
+	//fmt.Println(con.DumpConversation())
 
 }
