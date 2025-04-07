fe8e17e8337ce675c842264f35a558a37f29d989
Alexander Heier <alexander@engebrigtsenheier.no>
2025-04-07 11:25:34 +0200

    Update provider_ollama.go

--- provider_ollama.go
+++ provider_ollama.go
@@ -266,7 +266,7 @@ func convertHistoryToOllama(h []HistoryStruct) []ollamaMessage {
 	if rag {
 		array = append(array, ollamaMessage{
 			Role:    "system",
-			Content: GetRAG(h[len(h)-1].Content),
+			Content: "This is innformation from RAG: " + GetRAG(con.History[len(con.History)-1].Content)})
 		})
 	}
 	return array
